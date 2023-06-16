0b87cffe05ae8c309c7e6a56b81537a12629ce44
malhee <emilianodicampli@gmail.com>
2023-06-16 18:29:30 -0300

    --

--- cars/models.py
+++ cars/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
+def validate_price(price):
+    if price < 0:
+        raise ValidationError("Invalid price!")
 
 
 class Car(models.Model):
